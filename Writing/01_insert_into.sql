4e3aff5e7d9cd9b430dddc22ff8c903bb9371081
Camilo <camilovargas300@gmail.com>
2025-07-29 15:52:28 +1000

    SQL - Clase 3

---
+++ Writing/01_insert_into.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id, name, surname) VALUES (8, 'Maria', 'Lopez')
+
+INSERT INTO users (user_id, name, surname) VALUES ('Paco', 'Perez')
---
+++ Writing/02_update.sql
@@ -0,0 +1,3 @@
+UPDATE users SET age = '21' WHERE user_id = 8
+
+UPDATE users SET age = '20', init_date = '2025-10-12' WHERE user_id = 8
